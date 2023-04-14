87a55ead11e615aae7629187a76ccb34aacbf471
KesterDaniel <kesterdan17@gmail.com>
2023-04-14 12:23:48 +0100

    authenticating users

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 ##CREATE TABLE IN DB
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,6 +22,24 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
 
+# User loader
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.get(User, str(user_id))
+
+
+# fetching user and checking password match
+def get_user(email, password):
+    """
+    email and password refers to the data in the login form
+    which will be used to find the user in the database
+    returns None no user is found
+    """
+    user = User.query.filter_by(email=email).first()
+    password_match = check_password_hash(user.password, password)
+    if user is not None and password_match:
+        return user
+    return None
 
 
 @app.route('/')
@@ -44,27 +65,39 @@ def register():
         except IntegrityError:
             db.session.rollback()
             return "Sorry use a different email"
-        return redirect(url_for("secrets", user=request.form["name"]))
+        login_user(new_user)
+        return redirect(url_for("secrets"))
 
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == "POST":
+        user = get_user(request.form["email"], request.form["password"])
+        if user is not None:
+            login_user(user)
+            return redirect(url_for("secrets"))
+        else:
+            return "User not Found"
     return render_template("login.html")
 
 
-@app.route('/secrets/<user>')
-def secrets(user):
-    return render_template("secrets.html", user=user)
+@app.route('/secrets')
+@login_required
+def secrets():
+    return render_template("secrets.html", current_user=current_user)
 
 
 @app.route('/logout')
+@login_required
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for("home"))
 
 
 @app.route('/download/<path:filename>')
+@login_required
 def download(filename):
     app.config['UPLOAD_FOLDER'] = "./static/files"
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
